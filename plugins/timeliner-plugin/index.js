6c2f2da1ec8daa0ed95ed9b069adafc4e79ea471
jbsiraudin <jb.siraudin@gmail.com>
2025-10-04 00:05:38 +0200

    Update timeliner plugin with correct webpack configuration

--- plugins/timeliner-plugin/index.js
+++ plugins/timeliner-plugin/index.js
@@ -4,5 +4,20 @@ module.exports = function timelinerPlugin(context, options) {
     getClientModules() {
       return [require.resolve('./src/client-modules/timeliner')];
     },
+    configureWebpack(config, isServer) {
+      if (isServer) {
+        // Exclude Paper.js and its dependencies from server-side bundle
+        return {
+          externals: [
+            {
+              'paper': 'paper',
+              'canvas': 'canvas',
+              'jsdom': 'jsdom'
+            }
+          ]
+        };
+      }
+      return {};
+    },
   };
 };
